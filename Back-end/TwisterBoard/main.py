32ad6cbfbd7e4c0b174e37aa52a8e2cb101e6d5f
GeeraertHerber <herber.geeraert@student.howest.be>
2020-12-30 13:40:43 +0000

    WIP: working eventlisteners

--- Back-end/TwisterBoard/main.py
+++ Back-end/TwisterBoard/main.py
@@ -4,24 +4,32 @@ import time
 
 class TwisterBoard(object):
     """docstring for TwisterBoard."""
-    def __init__(self, arg):
+    def __init__(self):
         super(TwisterBoard, self).__init__()
-        self.listInputs = [2]
+        self.listInputs = [16]
     
     def setup(self):
+        io.setwarnings(False)
         io.setmode(io.BCM)
         for x in self.listInputs:
             io.setup(x, io.IN, pull_up_down=io.PUD_DOWN)
 
+    def cleanup(self):
+        io.cleanup()
+
     # Callbacks
 
     def buttonUnPressed(self, channel):
-        io.remove_event_detect(channel)
-        print("button Released")
+        print("Button released")
+
+    def buttonAction(self, channel):
+        if (io.input(channel)):
+            self.buttonPressed(channel)
+        else: 
+            self.buttonUnPressed(channel)
 
     def buttonPressed(self, channel):
-        print("button pressed")
-        io.add_event_detect(x, io.FALLING, callback=self.buttonUnPressed)
+        print("Button pressed")
 
 
     # Eventlisteners
@@ -34,8 +42,23 @@ class TwisterBoard(object):
             self.removeOneListener(x)
 
     def createOneListener(self, button):
-        io.add_event_detect(button, io.RISING, callback=self.buttonPressed)
+        io.add_event_detect(button, io.BOTH, callback=self.buttonAction, bouncetime=200)
 
     def createAllListeners(self):
+        print("Creating listeners")
         for x in self.listInputs:
             self.createOneListener(x)
+        print("Created listeners")
+
+
+
+if __name__ == "__main__":
+    twister = TwisterBoard()
+    twister.setup()
+    twister.createAllListeners()
+    try:
+        while (1):
+            pass
+    except KeyboardInterrupt:
+        twister.removeAllListeners()
+        twister.cleanup()
