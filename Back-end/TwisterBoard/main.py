5ba492a488d188630271628a563b284e5d413f58
GeeraertHerber <herber.geeraert@student.howest.be>
2020-12-30 21:18:36 +0000

    REM: whitespace

--- Back-end/TwisterBoard/main.py
+++ Back-end/TwisterBoard/main.py
@@ -24,23 +24,24 @@ def connect():
 
 def on_message(client, userdata, msg):
     message = json.loads(str(msg.payload.decode("utf-8")))
-    print(message["color"])
+    print(message["limb"])
+    limb = message["limb"]
     if int(message["color"]) == 0:
         print("Color Red")
         for x in twister._color_list[0]:
-            twister.createOneListener(x)
+            twister.createOneListener(x, limb)
     elif int(message["color"]) == 1:
         print("Color Blue")
         for x in twister._color_list[1]:
-            twister.createOneListener(x)
+            twister.createOneListener(x, limb)
     elif int(message["color"]) == 2:
         print("Color Green")
         for x in twister._color_list[2]:
-            twister.createOneListener(x)
+            twister.createOneListener(x, limb)
     elif int(message["color"]) == 3:
         print("Color Yellow")
         for x in twister._color_list[3]:
-            twister.createOneListener(x)
+            twister.createOneListener(x, limb)
     else:
         pass
 
@@ -48,16 +49,19 @@ def on_message(client, userdata, msg):
 
 
 # Main
+try:
+    twister = TwisterBoard()
+    twister.setup()    
+    twister.removeAllListeners()
 
-twister = TwisterBoard()
-twister.setup()    
-twister.removeAllListeners()
-
-client = mqtt.Client()
-client.on_message = on_message
-connect()
-client.subscribe('/mat/01/#')
-client.loop_forever()
+    client = mqtt.Client()
+    client.on_message = on_message
+    connect()
+    client.subscribe('/mat/01/#')
+    client.loop_forever()
+except KeyboardInterrupt as e:
+    twister.cleanup()
+    client.disconnect()
 
 
 
