8e1d0a51c0c54075bed7c66923161b6b7561040e
maximweewauters <43748399+maximweewauters@users.noreply.github.com>
2021-01-06 13:29:58 +0100

    player page + css

--- webapp/docker/webapp/app/static/js/templates.js
+++ webapp/docker/webapp/app/static/js/templates.js
@@ -9,24 +9,24 @@ timer_startgame = 5
 #Game Templates
 \*------------------------------------*/
 
-const Temp_SelectPlayers = function(){
-    document.querySelector('#gamewindow').innerHTML = ` 
-    <div id="playerfields">    
-        <div class="o-row">
-            <label for="playername">name:</label>
-            <input type="text" name="playername">
-        </div> 
-    <div>
+const Temp_SelectPlayers = function(){ //later terug zetten, sgewoon efkes voor beter html te kunnen lezen
+    // document.querySelector('#gamewindow').innerHTML = ` 
+    // <div id="playerfields">    
+    //     <div class="o-row">
+    //         <label for="playername">name:</label>
+    //         <input type="text" name="playername">
+    //     </div> 
+    // <div>
     
-    `;
-    document.querySelector('#gamewindow').innerHTML += ` 
-    <div class="o-row" id="addplayerfield">
-        <button type="button" onclick="AddPlayer();">Add Player</button>
-    </div> 
-    <div class="o-row">
-        <button type="button" id="BtnSelectPlayers">Next</button>
-    </div> 
-    `;
+    // `;
+    // document.querySelector('#gamewindow').innerHTML += ` 
+    // <div class="o-row" id="addplayerfield">
+    //     <button type="button" onclick="AddPlayer();">Add Player</button>
+    // </div> 
+    // <div class="o-row">
+    //     <button type="button" id="BtnSelectPlayers">Next</button>
+    // </div> 
+    // `;
     document.querySelector("#BtnSelectPlayers").addEventListener("click", function() {ValidatePlayers();});
 }
 
@@ -132,8 +132,9 @@ const Temp_EndGame = function(){
 #Template functions
 \*------------------------------------*/
 
-const AddPlayer = function(){
-    if (player_count < 4) {
+const AddPlayer = function(NumberPlayers){
+    console.log(NumberPlayers)
+    if(NumberPlayers >= player_count){
         let playerfield = document.getElementById("playerfields");
         //create div element and place it in id
         let row = document.createElement("div");
@@ -142,33 +143,64 @@ const AddPlayer = function(){
 
         //create label/input + details and place it in div 
         let lbl = document.createElement("label");
-        lbl.innerHTML = "name: ";
+        lbl.innerHTML = "name player: ";
         lbl.setAttribute("for", "playername");
         row.appendChild(lbl);
 
         let inpt = document.createElement("input");
         inpt.setAttribute("type", "text");
         inpt.setAttribute("name", "playername");
+        inpt.setAttribute("placeholder", "name...");
+        inpt.classList.add("c-inputplayer");
         row.appendChild(inpt);
-
-        //oude code
-        // document.getElementById('playerfields').innerHTML += `
-        //     <div class="o-row">
-        //         <label for="playername">name:</label>
-        //         <input type="text" name="playername">
-        //     </div> 
-        // `;
-        if (player_count == 3) {
-            let addplayerfield = document.getElementById("addplayerfield");
-            if (addplayerfield.style.display === "none") {
-                addplayerfield.style.display = "block";
-            } else {
-                addplayerfield.style.display = "none";
-            }
-        }
         player_count++;
     }
     else{
-        alert("max players reached")
+        let elems = document.getElementsByClassName("o-row");
+        var len = elems.length;
+        var lastelement = len < 1 ? "" : elems[len-1];
+        lastelement.remove()
+        player_count--;
     }
+    
+
+
+    //oude code
+    
+    // if (player_count < 4) {
+    //     let playerfield = document.getElementById("playerfields");
+    //     //create div element and place it in id
+    //     let row = document.createElement("div");
+    //     row.classList.add("o-row");
+    //     playerfield.appendChild(row);
+
+    //     //create label/input + details and place it in div 
+    //     let lbl = document.createElement("label");
+    //     lbl.innerHTML = "name: ";
+    //     lbl.setAttribute("for", "playername");
+    //     row.appendChild(lbl);
+
+    //     let inpt = document.createElement("input");
+    //     inpt.setAttribute("type", "text");
+    //     inpt.setAttribute("name", "playername");
+    //     row.appendChild(inpt);
+
+    //     //oude code
+    //     // document.getElementById('playerfields').innerHTML += `
+    //     //     <div class="o-row">
+    //     //         <label for="playername">name:</label>
+    //     //         <input type="text" name="playername">
+    //     //     </div> 
+    //     // `;
+    //     if (player_count == 3) {
+    //         let addplayerfield = document.getElementById("addplayerfield");
+    //         if (addplayerfield.style.display === "none") {
+    //             addplayerfield.style.display = "block";
+    //         } else {
+    //             addplayerfield.style.display = "none";
+    //         }
+    //     }
+    //    player_count++;
+    //}
+    
 }
