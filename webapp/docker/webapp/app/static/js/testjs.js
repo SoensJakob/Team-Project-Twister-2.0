fe115af7e26f91096e061b4908793b682bc7bf67
Ewout Deschuymere <55757656+EwoutDes@users.noreply.github.com>
2021-01-20 20:47:16 +0100

    TST: moveplayer

--- webapp/docker/webapp/app/static/js/testjs.js
+++ webapp/docker/webapp/app/static/js/testjs.js
@@ -1,30 +1,89 @@
 'use strict'
 
-const StartGameTimer = 5;
-let memoryarr = {"row": [], "col": []};
-let memoryindex = 0;
-let timeleft = 0;
-let out_msg = '{"buttonpressed":[{"row": "", "col": ""}]}';
-let mqttmssg = [];
+let twisterboard = [
+    [["robbe","left","hand"],["","",""],["","",""],["","",""],["","",""],["","",""]],
+    [["shiangi","left","hand"],["","",""],["","",""],["","",""],["","",""],["","",""]],
+    [["","",""],["shiangi","left","hand"],["","",""],["","",""],["","",""],["","",""]],
+    [["","",""],["","",""],["","",""],["","",""],["","",""],["","",""]]
+];
+
+let iets = {"playerinfo":[{"name":"a","score":0,"alive":0},{"name":"b","score":0,"alive":0},{"name":"c","score":0,"alive":1}]}
+let gamecolors = ["red", "blue", "yellow", "green"];
+let bodyparts = ["left hand", "left foot", "right foot", "right hand"];
+let mqttmssg = {"":[{"row": "", "col": ""}]};
+let currplayerindex = 0;
+let arraybodypart = ["left", "hand"]
+let gamemode = "twister";
+
 
 document.addEventListener('DOMContentLoaded', function() {
-    console.log('DOM geladen');
-    init();
+    // console.log(twisterboard.length);
+    // let players = iets['playerinfo'];
+    // let randindex = Math.floor(Math.random() * gamecolors.length)
+    // //init();
+    // let newmqqtmssg = setInterval(function name(params) {
+    //     mqttmssg = {"":[{"row": Math.floor(Math.random() * 4) + 1, "col": Math.floor(Math.random() * 6) + 1}]};
+    // }, 1000);
+    console.log(searchplayer("robbe", arraybodypart));
+
 });
 
 const init = function(){
-    let mqttobj = JSON.parse(out_msg);
-    switch (Object.keys(mqttobj)[0]) {
-        case 'buttonpressed':
-            mqttmssg = ["pressed", mqttobj.buttonpressed[0]];
-            break;
-        case 'buttonreleased':
-            mqttmssg = ["released", mqttobj.buttonreleased[0]];
-            break;
-    
-        default:
-            console.log("game - game-mechanics error: mqttobj error in setoutmsg");
-            break;
+    let randbodypart = bodyparts[Math.floor(Math.random() * 4)];
+    let arrbodypart = randbodypart.split(" ");
+    let randcolor = GetTwisterColor();
+
+
+    let timeleft = 5;
+    let timernextplayer = setInterval(function(){
+        if (timernextplayer == 0) {
+            moveplayer(mqttmssg[1], players[currplayerindex], randcolor, arrbodypart);
+            randbodypart = bodyparts[Math.floor(Math.random() * 4)];
+            arrbodypart = randbodypart.split(" ");
+            randcolor = GetTwisterColor();
+            timeleft = 5
+        }
+        timeleft -= 1;
+    }, 1000);
+}
+
+const moveplayer = (mqttmssg, playername, randcolor, arrbodypart) => {
+    let playerexist = searchplayer();
+}
+
+const addplayeronfield = (color, limb) => {
+
+}
+
+const searchplayer = (playername, arraybodypart) =>{
+    let isfound = false;
+    let returnmsg = [];
+    for (let i = 0; i < twisterboard.length; i++) {
+        for (let j = 0; j < twisterboard[i].length; j++) {
+            if (playername ==  twisterboard[i][j][0] && arraybodypart[0] == twisterboard[i][j][1] && arraybodypart[1] == twisterboard[i][j][2] ) {
+                returnmsg.push(i,j,twisterboard[i][j][0],twisterboard[i][j][1],twisterboard[i][j][2]);
+                isfound = true;
+            }
+        }
+        
     }
-    console.log(mqttmssg);
+    if (!isfound) {
+        
+        return false
+    }
+    else{
+        return returnmsg
+    }
+}
+
+const GetTwisterColor = () => {
+    let countbtnused = 0;
+    let randindex = Math.floor(Math.random() * gamecolors.length)
+    twisterboard.forEach(row => {
+        if (row[randindex][0]) {
+            countbtnused++;
+        }
+    });
+    if (countbtnused == 6) {GetTwisterColor()}
+    else { return gamecolors[randindex]}
 }
