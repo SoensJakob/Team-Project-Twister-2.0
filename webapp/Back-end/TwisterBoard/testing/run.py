96ebed32fba3a036fb926bbe23e7a8815cd01afb
GeeraertHerber <herber.geeraert@student.howest.be>
2021-01-21 15:48:02 +0100

    Upd: testing all buttons

--- webapp/Back-end/TwisterBoard/testing/run.py
+++ webapp/Back-end/TwisterBoard/testing/run.py
@@ -5,9 +5,19 @@ color_list = [4, 17,27,22,10,9,11,0, 5, 6, 13,19, 26,21,20,16,12,1 ,7, 8, 25,24,
 io.setwarnings(False)
 io.setmode(io.BCM)
 for x in color_list:
-    io.setup(x, io.IN, io.PUD_UP)
+    io.setup(x, io.IN, io.PUD_OFF)
 
 for x in color_list:
     y = io.input(x)
     print(y, x)
     sleep(0.2)
+
+def callback(channel):
+    print(f"{channel} pressed")
+
+for x in color_list:
+    io.remove_event_detect(x)
+    io.add_event_detect(x, io.FALLING, callback=callback, bouncetime=150)
+
+while (1):
+    pass
